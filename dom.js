fe630c0b8b13ed1122a0dc0ccd1a0acc5254b158
NSINGHRAJPUT <nsinghrajput30@gmail.com>
2023-02-25 20:15:20 +0530

    second commit

--- dom.js
+++ dom.js
@@ -1,6 +1,15 @@
-var items = document.getElementsByClassName('list-group-item');
- items[2].style.backgroundColor='green';
- for(let i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold';
-    items[i].style.color='purple'
- }
+// var items = document.getElementsByClassName('list-group-item');
+//  items[2].style.backgroundColor='green';
+//  for(let i=0;i<items.length;i++){
+//     items[i].style.fontWeight='bold';
+//     items[i].style.color='purple'
+//  }
+var itemx=document.getElementsByClassName('item');
+console.log(itemx)
+itemx[0].style.color='yellow';
+
+
+
+var item=document.getElementsByTagName('li');
+console.log(item[4])
+item[4].style.backgroundColor='red'
