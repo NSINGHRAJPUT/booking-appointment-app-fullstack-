228efdbd68ae7b9346f76bfbde0d31f155e9b01b
NSINGHRAJPUT <nsinghrajput30@gmail.com>
2023-02-25 20:36:58 +0530

    third commit

--- dom.js
+++ dom.js
@@ -4,12 +4,20 @@
 //     items[i].style.fontWeight='bold';
 //     items[i].style.color='purple'
 //  }
-var itemx=document.getElementsByClassName('item');
-console.log(itemx)
-itemx[0].style.color='yellow';
+// var itemx=document.getElementsByClassName('item');
+// console.log(itemx)
+// itemx[0].style.color='yellow';
 
 
 
-var item=document.getElementsByTagName('li');
-console.log(item[4])
-item[4].style.backgroundColor='red'
+// var item=document.getElementsByTagName('li');
+// console.log(item[4])
+// item[4].style.backgroundColor='red'
+
+ var item =document.querySelectorAll('li:nth-child(2)');
+ item[0].style.color='green'
+
+ var odd=document.querySelectorAll('li:nth-child(odd)');
+ for(let i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green'
+ }
