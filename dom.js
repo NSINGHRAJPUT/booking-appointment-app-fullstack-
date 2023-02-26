b25a3b0be73f1631846fe773038750cb94e856d6
NSINGHRAJPUT <nsinghrajput30@gmail.com>
2023-02-26 19:08:50 +0530

    dompractice5

--- dom.js
+++ dom.js
@@ -9,6 +9,17 @@ var itemList = document.getElementById('items')
       //   console.log(full_name)
       var li = document.createElement('li');
       li.appendChild(document.createTextNode(`${full_name},${email_id},${phone_number}`))
+      var deleteBtn = document.createElement('button');
+   
+      // Add classes to del button
+      deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    
+      // Append text node
+      deleteBtn.appendChild(document.createTextNode('Delete'));
+ 
+      // Append button to li
+      li.appendChild(deleteBtn);
+     
       itemList.appendChild(li)
       var myobj={
          name:`${full_name}`,
@@ -21,5 +32,20 @@ var itemList = document.getElementById('items')
       console.log(myobj_deserialized)
    
    })
-    
+   itemList.addEventListener('click', removeItem);
+   // Delete event
+   
+  
+   
+   // Remove item
+   function removeItem(e){
+     if(e.target.classList.contains('delete')){
+       if(confirm('Are You Sure?')){
+         var li = e.target.parentElement;
+         itemList.removeChild(li);
+       }
+     }
+   }
+   
+  
    
