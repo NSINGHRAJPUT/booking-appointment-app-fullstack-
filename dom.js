962c4cf9533d937900eba51b76c19dd708a7f427
NSINGHRAJPUT <nsinghrajput30@gmail.com>
2023-02-26 21:07:55 +0530

    editingform

--- dom.js
+++ dom.js
@@ -6,20 +6,23 @@ var itemList = document.getElementById('items')
       var full_name=document.getElementById('name').value;
       var email_id=document.getElementById('email').value;
       var phone_number=document.getElementById('number').value;
-      //   console.log(full_name)
       var li = document.createElement('li');
       li.appendChild(document.createTextNode(`${full_name},${email_id},${phone_number}`))
-      var deleteBtn = document.createElement('button');
-   
-      // Add classes to del button
-      deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-    
-      // Append text node
-      deleteBtn.appendChild(document.createTextNode('Delete'));
- 
-      // Append button to li
-      li.appendChild(deleteBtn);
-     
+      // Create del button element
+  var deleteBtn = document.createElement('button');
+  // Create edit button element
+  var editBtn = document.createElement('button');
+  // Add classes to del button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  //add classes to edit button 
+  editBtn.className ='btn btn-warning btn-sm edit'
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('Delete'));
+  editBtn.appendChild(document.createTextNode('edit'));
+  // Append button to li
+  li.appendChild(editBtn)
+  li.appendChild(deleteBtn);
+  
       itemList.appendChild(li)
       var myobj={
          name:`${full_name}`,
@@ -31,12 +34,10 @@ var itemList = document.getElementById('items')
       var myobj_deserialized=JSON.parse(localStorage.getItem(`${email_id}`))
       console.log(myobj_deserialized)
    
-   })
+   
    itemList.addEventListener('click', removeItem);
-   // Delete event
-   
-  
-   
+   itemList.addEventListener('click', edititem)
+
    // Remove item
    function removeItem(e){
      if(e.target.classList.contains('delete')){
@@ -47,5 +48,19 @@ var itemList = document.getElementById('items')
      }
    }
    
+   // editing item
+
+   function edititem(e){
+      if(e.target.classList.contains('edit')){
+        var li = e.target.parentElement;
+         itemList.removeChild(li);
+         console.log(myobj)
+        localStorage.removeItem(email_id)
+         document.getElementById('name').value=myobj.name
+         document.getElementById('email').value=myobj.email_id
+         document.getElementById('number').value=myobj.number
+
+      }
+   }
   
-   
+})
