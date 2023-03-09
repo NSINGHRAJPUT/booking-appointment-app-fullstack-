7399bfd497ff1e01de15b2d1d45ccf51d4ca4fb9
NSINGHRAJPUT <nsinghrajput30@gmail.com>
2023-03-09 21:39:52 +0530

    Expense tracker

--- dom.js
+++ dom.js
@@ -1,66 +1,67 @@
-
-let btn=document.querySelector('.btn');
-var itemList = document.getElementById('items')
-   btn.addEventListener('click',(e)=>{
-      e.preventDefault();
-      var full_name=document.getElementById('name').value;
-      var email_id=document.getElementById('email').value;
-      var phone_number=document.getElementById('number').value;
-      var li = document.createElement('li');
-      li.appendChild(document.createTextNode(`${full_name},${email_id},${phone_number}`))
-      // Create del button element
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+itemList.addEventListener('click', editItem);
+var myobj={};
+var myobj1;
+// Add item
+function addItem(e){
+  e.preventDefault();
+  var newItem = document.getElementById('expense').value;
+  var newItem1 = document.getElementById('description').value;
+  var newItem2 = document.getElementById('details').value;
+  var li = document.createElement('li');
+  li.className = 'list-group-item';
+  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(" "));
+  li.appendChild(document.createTextNode(newItem1));
+  li.appendChild(document.createTextNode(" "));
+  li.appendChild(document.createTextNode(newItem2));
+  li.appendChild(document.createTextNode(" "));
   var deleteBtn = document.createElement('button');
-  // Create edit button element
   var editBtn = document.createElement('button');
-  // Add classes to del button
   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-  //add classes to edit button 
-  editBtn.className ='btn btn-warning btn-sm edit'
-  // Append text node
+  editBtn.className = 'btn btn-warning btn-sm edit';
   deleteBtn.appendChild(document.createTextNode('Delete'));
-  editBtn.appendChild(document.createTextNode('edit'));
-  // Append button to li
-  li.appendChild(editBtn)
+  editBtn.appendChild(document.createTextNode('Edit'))
   li.appendChild(deleteBtn);
-  
-      itemList.appendChild(li)
-      var myobj={
-         name:`${full_name}`,
-         email_id: `${email_id}`, 
-         number:`${phone_number}`
-      }
+  li.appendChild(editBtn);
+  itemList.appendChild(li);
+  myobj={
+    expense:`${newItem}`,
+    Description:`${newItem1}`,
+    Details:`${newItem2}`
+  }
       var myobj_serialized=JSON.stringify(myobj)
-      localStorage.setItem(`${email_id}`, myobj_serialized);
-      var myobj_deserialized=JSON.parse(localStorage.getItem(`${email_id}`))
-      console.log(myobj_deserialized)
-   
-   
-   itemList.addEventListener('click', removeItem);
-   itemList.addEventListener('click', edititem)
+      localStorage.setItem(`${newItem}`, myobj_serialized);
+      var myobj_deserialized=JSON.parse(localStorage.getItem(`${newItem}`))
+      myobj1=myobj_deserialized
+      console.log(myobj1)
+}
+// Remove item
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+function editItem(e){
+    if(e.target.classList.contains('edit')){
+        let x=e.target.parentElement.textContent
+        var num = `${x}`.match(/\d+/g);
+        let obj = JSON.parse(localStorage.getItem(`${num[0]}`))
+        console.log(obj.expense)
+        document.getElementById('expense').value=obj.expense;
+        document.getElementById('description').value = obj.Description;
+        document.getElementById('details').value =obj.Details;
+        localStorage.removeItem(num[0]);
+        var li=e.target.parentElement;
+        itemList.removeChild(li);
+    }
+}
 
-   // Remove item
-   function removeItem(e){
-     if(e.target.classList.contains('delete')){
-       if(confirm('Are You Sure?')){
-         var li = e.target.parentElement;
-         itemList.removeChild(li);
-       }
-     }
-   }
-   
-   // editing item
-
-   function edititem(e){
-      if(e.target.classList.contains('edit')){
-        var li = e.target.parentElement;
-         itemList.removeChild(li);
-         console.log(myobj)
-        localStorage.removeItem(email_id)
-         document.getElementById('name').value=myobj.name
-         document.getElementById('email').value=myobj.email_id
-         document.getElementById('number').value=myobj.number
-
-      }
-   }
-  
-})
